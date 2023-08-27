50f4dd164d5752d1c4de05ed36da1de4844d389d
Fuy98 <79111629+Fuy98@users.noreply.github.com>
2023-08-26 20:27:12 -0600

    new

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e69bd0b2fe4986cc816c3f131bccfc2d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "9dbfd542101d43fc91e33acfc55d2b2c"
